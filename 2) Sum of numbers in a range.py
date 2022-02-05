e5d16d1ec1dbc6863898f07ba22563b7f1b4f8e6
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-02-05 16:45:42 +1300

    03 Level 1 recap - revision

--- 2) Sum of numbers in a range.py
+++ 2) Sum of numbers in a range.py
@@ -4,5 +4,5 @@ num_range = [20, 21, 22, 23, 24, 25]
 # Prints the numbers in the list
 for sum_of_num in num_range:
     print(sum_of_num)
-# Adds the numbers in the list together and prints the sum at the end
+# Adds the numbers in the list together (sum) and prints it at the end
 print(20 + 21 + 22 + 23 + 24 + 25)
