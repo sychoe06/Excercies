2f4507fc36aa4d7fbd69b4a0c2ddc859bddf638b
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-02-05 16:43:48 +1300

    03 Level 1 recap - revision Exercise #1 (biggest number)

--- 1) Bigest number.py
+++ 1) Bigest number.py
@@ -1,3 +1,4 @@
+# Integer checking function
 def integer_checker(question):
     error = "\nSorry, you must enter an integer\n"
     number = ""
@@ -11,17 +12,21 @@ def integer_checker(question):
 
 # Main routine
 zero = "no"
+# Keeps asking for two numbers until one of the numbers is 0
 while zero == "no":
     num_1 = integer_checker("First number: ")
     num_2 = integer_checker("Second number: ")
+    # If the first number is bigger than the second number, it will print the first number
     if num_1 > num_2:
         print(num_1)
+        # If either the first or second number = 0 then the code will stop looping
         if num_1 == 0:
             zero = "yes"
         elif num_2 == 0:
             zero = "yes"
         else:
             print()
+    # If the second number is bigger than the first number, it will print the second number
     elif num_1 < num_2:
         print(num_2)
         if num_1 == 0:
@@ -30,6 +35,7 @@ while zero == "no":
             zero = "yes"
         else:
             print()
+    # However if no number is bigger than the other it will say that both numbers are equal
     else:
         print("The two numbers are equal")
         if num_1 == 0:
---
+++ 2) Sum of numbers in a range.py
@@ -0,0 +1,8 @@
+# List for the number range from 20 to 25 (inclusive)
+num_range = [20, 21, 22, 23, 24, 25]
+
+# Prints the numbers in the list
+for sum_of_num in num_range:
+    print(sum_of_num)
+# Adds the numbers in the list together and prints the sum at the end
+print(20 + 21 + 22 + 23 + 24 + 25)
