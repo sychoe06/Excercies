af21b620fd38ab80cb98cbd087ad61ffab2ac23a
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-02-08 14:06:58 +1300

    03 Level 1 recap - revision

---
+++ 7) Thunder and lightning.py
@@ -0,0 +1,3 @@
+sound = 340/1000  # converting 340 m/s to km instead
+count = float(input("Enter the number of seconds between the lightning and thunder: "))
+print("The distance of the storm is about", round(sound * count, 2), "km.")
