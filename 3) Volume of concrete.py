e3d8395e27565ee6886197a2da69822582029377
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-02-08 14:20:56 +1300

    03 Level 1 recap - revision

---
+++ 3) Volume of concrete.py
@@ -0,0 +1,6 @@
+building_type = input("What is the type of the building? \n"
+                      "Enter '1' for Residential or '2' for Commercial: ")
+length = input("\nEnter the length of the building: ")
+width = input("\nEnter the width of the building: ")
+if building_type == "1":
+    print()
