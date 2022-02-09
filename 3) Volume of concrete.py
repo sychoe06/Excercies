d2e46d1ab15f87f38b6f76d3091ff704b103cb93
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-02-10 10:33:06 +1300

    03 Level 1 recap - revision
    10/02/2022 (2)

--- 3) Volume of concrete.py
+++ 3) Volume of concrete.py
@@ -28,20 +28,23 @@ building_error = "Please enter only '1', '2' or 'x'!"
 
 while end_of_program == "False":
     building_type = input("What is the type of the building?\n"
-                          "Enter...\n '1' for Residential\n'2' for Commercial"
-                          "\n'x' to finish: ").lower()
-    length = length_num_checker("\nEnter the length of the building: ")
-    width = width_num_checker("\nEnter the width of the building: ")
+                          "'1' for Residential\n'2' for Commercial"
+                          "\n'x' to finish\nEnter here: ").lower()
+    length = length_num_checker("\nEnter the length of the building "
+                                "(in metres): ")
+    width = width_num_checker("Enter the width of the building"
+                              "(in metres): ")
     volume_1 = length * width * DEPTH_1
     volume_2 = length * width * DEPTH_2
+
     if building_type == "1":
         print("\nThe volume of concrete required for a slab with a length of",
               length, "and width of", width, "and a depth of", DEPTH_1, "is",
-              volume_1, "cubic metres")
+              volume_1, "cubic metres\n")
     elif building_type == "2":
         print("\nThe volume of concrete required for a slab with a length of",
               length, "and width of", width, "and a depth of", DEPTH_2, "is",
-              volume_2, "cubic metres")
+              volume_2, "cubic metres\n")
     elif building_type == "x":
         total = volume_1 + volume_2
         print("The total amount of concrete needed for the day is", total)
